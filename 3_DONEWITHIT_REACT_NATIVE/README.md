b50383f46f5831910fb39643cc11c8450974c447
Abdur Rahim <abdoerrahiem@gmail.com>
2020-07-31 21:55:42 +0700

    added offline supports

--- 3_DONEWITHIT_REACT_NATIVE/README.md
+++
@@ -1,3 +0,0 @@
-<h1>DoneWithIt App</h1>
-
-<p>Created using React Native</p>
--- 3_DONEWITHIT_REACT_NATIVE/api/client.js
+++
@@ -1,7 +0,0 @@
-import { create } from 'apisauce'
-
-const apiClient = create({
-  baseURL: 'http://192.168.100.57:9000/api',
-})
-
-export default apiClient
